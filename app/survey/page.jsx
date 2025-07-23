df15a8da657144f98d9243565da4f608574ca952
jobigboss <josursit@gmail.com>
2025-07-23 18:31:07 +0700

    up

--- app/survey/page.jsx
+++ app/survey/page.jsx
@@ -1,6 +1,8 @@
 "use client";
 
-import SurveyMenu from "./components/SurveyMenu";
+import dynamic from "next/dynamic";
+
+const SurveyMenu = dynamic(() => import("./components/SurveyMenu"), { ssr: false });
 
 export default function SurveyPage() {
   return <SurveyMenu />;
