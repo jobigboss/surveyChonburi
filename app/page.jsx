6062b956a87d39e45be107510c70233c4f510a21
jobigboss <josursit@gmail.com>
2025-07-23 22:24:55 +0700

    update 2224

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import { Eye, EyeOff } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 
 const foremostBlue = "#0094E5";
 const foremostOrange = "#FF9100";
